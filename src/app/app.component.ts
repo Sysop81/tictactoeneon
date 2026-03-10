c170857a887a5ab0ee53f5fea18303c8aded65f4
José Ramón López <jose.rlg27@gmail.com>
2026-03-10 18:47:45 +0100

    first version of tictactoe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,57 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'tictactoe';
+  winningCombos = [
+    // Rows
+    [[0,0],[0,1],[0,2]],
+    [[1,0],[1,1],[1,2]],
+    [[2,0],[2,1],[2,2]],
+
+    // Cols
+    [[0,0],[1,0],[2,0]],
+    [[0,1],[1,1],[2,1]],
+    [[0,2],[1,2],[2,2]],
+
+    // Cross
+    [[0,0],[1,1],[2,2]],
+    [[0,2],[1,1],[2,0]],
+  ];
+  isPlayerOne :boolean = true;
+  isWinner : boolean = false;
+  table = signal([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]);
+
+  cellClick(i : number, x : number){
+    console.log("Pulsada: " + i + "-" + x);
+    if(this.table()[i][x] != '') return;
+
+    this.table.update(table =>{
+      const newTable = table.map(row => [...row]);
+      newTable[i][x] = this.isPlayerOne ? 'X' : 'O';
+      return newTable;
+    });
+    this.checkWinner();
+    
+  }
+
+  checkWinner(){
+    let valueChecked = this.isPlayerOne ? "X" : "O"
+    for(const combo of this.winningCombos){
+      const [a, b, c] = combo;
+      if (this.table()[a[0]][a[1]] == valueChecked &&
+          this.table()[b[0]][b[1]] == valueChecked &&
+          this.table()[c[0]][c[1]] == valueChecked){
+          console.log("Winner " + (this.isPlayerOne ? "X" : "O"));
+          this.isWinner = true
+          break;
+        }
+    }
+
+    if (!this.isWinner) this.isPlayerOne = !this.isPlayerOne;
+
+  }
+
 }
