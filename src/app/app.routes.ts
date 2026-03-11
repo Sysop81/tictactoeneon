4759ab16a2c60b3c68c85871a500576617757401
José Ramón López <jose.rlg27@gmail.com>
2026-03-11 17:28:39 +0100

    Adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+// My Game components
+import { LoginComponent } from './components/login/login.component';
+import { GameComponent } from './components/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'game', component: GameComponent},
+
+    // Default path
+    { path:'', redirectTo:'/login', pathMatch:'full' },
+    { path:'**', redirectTo:'/login' }
+];
