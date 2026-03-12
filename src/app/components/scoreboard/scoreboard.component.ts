514cca22dc8d6f654d7b1e4673ccdf57cffd9686
José Ramón López <jose.rlg27@gmail.com>
2026-03-12 16:14:26 +0100

    Adding socreboard compoent and working with internal component css

---
+++ src/app/components/scoreboard/scoreboard.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { GameManagerService } from '../../services/game-manager.service';
+
+@Component({
+  selector: 'app-scoreboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './scoreboard.component.html',
+  styleUrl: './scoreboard.component.css'
+})
+export class ScoreboardComponent {
+  // Inject the gameManager service
+    private gameService = inject(GameManagerService);
+    
+    // Get players names
+    public playerOne = this.gameService.playerOne;
+    public playerTwo = this.gameService.playerTwo;
+
+    // Get scores
+    public p1Score = this.gameService.playerOneScore;
+    public p2Score = this.gameService.playerTwoScore;
+}
