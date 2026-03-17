2146fac023f18c5524f4b76a6e7f7468c43072af
José Ramón López <jose.rlg27@gmail.com>
2026-03-17 14:04:27 +0100

    Adding storage service

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, signal } from '@angular/core';
+
+export interface GameState {
+  board: string[][];
+  currentTurn: 'X' | 'O';
+  movements: number;
+  pNames: {x:string; o: string};
+  scores: { x: number; o: number };
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  private readonly STATE_NAME : string  = 'state';
+
+  saveGame(state: GameState){
+    localStorage.setItem(this.STATE_NAME,JSON.stringify(state));
+  }
+
+  getCurrentGame<T>(): T | null {
+    const data = localStorage.getItem(this.STATE_NAME);
+    return data ? (JSON.parse(data) as T) : null;
+  }
+
+  clearGameData(){
+    localStorage.removeItem(this.STATE_NAME)
+  }
+
+  constructor() { }
+}
